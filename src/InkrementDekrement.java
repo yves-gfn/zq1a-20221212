316cbca78503863e3ead602b980a5cd5fb62a08c
sevysch3t-dev <yvest.dev@gmail.com>
2022-12-13 09:52:08 +0100

    new src inkrement dekrement file

---
+++ src/InkrementDekrement.java
@@ -0,0 +1,21 @@
+
+public class InkrementDekrement {
+
+	public static void main(String[] args) {
+		//Postfix
+		//Inkrement / Dekrement VOR der Variable bedeutet ERHÖHUNG bzw. VERRINGERUNG VOR der Zuweisung (oder weiterer Rechnung)
+		int a = 8;
+		int b = ++a;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		int c = 8;
+		int d = c++;
+		
+		System.out.println(c);
+		System.out.println(d);
+
+	}
+
+}
