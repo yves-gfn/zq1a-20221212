38072ca29278f16ae38b21ae54426d366541595a
sevysch3t-dev <yvest.dev@gmail.com>
2022-12-13 18:03:09 +0100

    last changes and additions

---
+++ src/_011_InkrementDekrement.java
@@ -0,0 +1,27 @@
+
+public class InkrementDekrement {
+
+	public static void main(String[] args) {
+		//Präfix
+		//Inkrement / Dekrement VOR der Variable bedeutet ERHÖHUNG bzw. VERRINGERUNG VOR der Zuweisung (oder weiterer Rechnung)
+		int a = 8;
+		int b = ++a;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+		//Postfix
+		//Inkrement / Dekrement wird erst NACH der Zuweisung (weitere Berechnung) ERHÖHT oder VERRINGERT
+		int c = 8;
+		int d = c++;
+		
+		System.out.println(c);
+		System.out.println(d);
+		
+		a++; //kein Problem!
+		c--; //kein Problem!
+
+	}
+
+}
