24b07fd9fc236e5d850361f3d8fb1387ab23ce8c
sevysch3t-dev <yvest.dev@gmail.com>
2022-12-13 11:06:14 +0100

    add ShortCircuitEvaluationRedFlag

---
+++ src/ShortCircuitEvaluationRedFlag.java
@@ -0,0 +1,16 @@
+
+public class ShortCircuitEvaluationRedFlag {
+
+	public static void main(String[] args) {
+
+		int i = 0;
+		int j = 0;
+		
+		while(i < 10 || j++ < 6) {
+			System.out.println("i = " + i);
+			i++;
+			System.out.println("j = " + j);
+		}
+	}
+
+}
