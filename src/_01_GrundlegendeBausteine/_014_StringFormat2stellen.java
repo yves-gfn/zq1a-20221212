f692847bf52bfcac97126547780a210435ec7c5c
sevysch3t-dev <yvest.dev@gmail.com>
2022-12-13 13:47:58 +0100

    StringFormat2Stellen

---
+++ src/_01_GrundlegendeBausteine/_014_StringFormat2stellen.java
@@ -0,0 +1,10 @@
+package _01_GrundlegendeBausteine;
+
+public class _014_StringFormat2stellen {
+
+	public static void main(String[] args) {
+		double zahl1 = 2.4534653463546345;
+		System.out.println("Mein Wert: " + String.format("%.2f", zahl1));
+
+	}
+}
