7277e511532b3849359ff5b9113f5bdda71cfff4
sevysch3t-dev <yvest.dev@gmail.com>
2022-12-13 14:35:30 +0100

    added exercise DurchschnittlicheNiederschlagsmenge

---
+++ src/_01_GrundlegendeBausteine/_Uebungen/_010_DurchschnittlicheNiederschlagsmenge.java
@@ -0,0 +1,29 @@
+package _01_GrundlegendeBausteine._Uebungen;
+
+import javax.swing.JOptionPane;
+
+public class _010_DurchschnittlicheNiederschlagsmenge {
+
+	public static void main(String[] args) {
+		
+		final int ANZAHL_MONATE = 3;
+		
+//		Friendly Reminder
+//		String mai = JOptionPane.showInputDialog("Geben Sie bitte die Niederschlagsmenge fuer Main ein: ");
+//		double umgMai = Double.parseDouble(mai);
+		
+		double april = Double.parseDouble(JOptionPane.showInputDialog
+				("Geben Sie bitte die Niederschlagsmenge fuer April ein: "));
+		double mai = Double.parseDouble(JOptionPane.showInputDialog
+				("Geben Sie bitte die Niederschlagsmenge fuer Mai ein: "));
+		double juni = Double.parseDouble(JOptionPane.showInputDialog
+				("Geben Sie bitte die Niederschlagsmenge fuer Juni ein: "));
+		
+		double result = (april + mai + juni) / ANZAHL_MONATE;
+		
+		JOptionPane.showMessageDialog(null, "Die Niederschlagsmenge fuer April is: " + String.format("%.0f l/qm", april) + "\n"
+									+ "Die Niederschlagsmenge fuer Mai ist: " + String.format("%.0f l/qm", mai) + "\n"
+									+ "Die Niederschlagsmenge fuer Juni ist: " + String.format("%.0fl/qm", juni) + "\n\n"
+									+ "Die durchschnittliche Niederschlagsmenge betraegt: " + String.format("%.2f l/qm", result));
+	}
+}
---
+++ src/_01_GrundlegendeBausteine/_Uebungen/_011_RechteckBerechnung.java
@@ -0,0 +1,10 @@
+package _01_GrundlegendeBausteine._Uebungen;
+
+public class _011_RechteckBerechnung {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
